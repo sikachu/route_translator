1958db20ff0f07fbbb782f744b68a73fdd10bbfc
Geremia Taglialatela <tagliala.dev@gmail.com>
2017-11-12 17:19:32 +0100

    Remove localized engine support
    The current engine implementation is buggy and incomplete.
    
    We are going to drop the whole feature, waiting for a new contributor
    
    Fixes #166, fixes #172, fixes #173 and fixes #178

--- test/dummy/app/controllers/blorgh/posts_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Blorgh
-  class PostsController < ActionController::Base
-    def index
-      render plain: I18n.locale
-    end
-  end
-end
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount Blorgh::Engine, at: '/blorgh'
-
   localized do
     get 'dummy',  to: 'dummy#dummy'
     get 'show',   to: 'dummy#show'
@@ -13,14 +11,10 @@ Rails.application.routes.draw do
     get 'prefixed_optional(/p-:page)', to: 'dummy#prefixed_optional', as: :prefixed_optional
 
     get ':id-suffix', to: 'dummy#suffix'
-
-    mount Blorgh::Engine, at: '/blorgh'
   end
 
   get 'partial_caching', to: 'dummy#partial_caching'
   get 'native', to: 'dummy#native'
-  get 'engine_es', to: 'dummy#engine_es'
-  get 'engine', to: 'dummy#engine'
 
   root to: 'dummy#dummy'
 end
